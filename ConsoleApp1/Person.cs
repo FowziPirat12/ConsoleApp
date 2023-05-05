2c63bcf78d7f01ba258bec24751c2a4622e328c8
FowziPirat12 <fowzino16@gmail.com>
2023-05-05 10:49:59 +0200

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private string name;
+        private int ålder;
+
+        
+        public string Myperson
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Myperson1
+        {
+            get { return ålder; }
+            set { ålder = value; }
+        }
+
+        public Person(string n, int å)
+        {
+
+            name = n;
+            ålder = å;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+using System;
+namespace ___
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person ("Fowzi", 18);
+
+            Console.WriteLine(p1.Myperson);
+            Console.WriteLine(p1.Myperson1);
+
+        }
+    }
+}
